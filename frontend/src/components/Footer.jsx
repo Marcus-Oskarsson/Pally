8848f7fad7d103073f7e48a1ba8d2bc08a37725e
Marcus Oskarsson <35783488+Marcus-Oskarsson@users.noreply.github.com>
2023-11-14 11:57:52 +0100

    Simple footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,44 @@
+import { Link } from 'react-router-dom';
+
+import Logo from './icons/Logo';
+
+import '../styles/footer.scss';
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className='page-footer'>
+      <div className='logo-container'>
+        <Link to='/'>
+          <Logo width={32} height={32} />
+        </Link>
+        <span>© 2023 Pally</span>
+      </div>
+      <div className='link-container'>
+        <nav className='footer-nav'>
+          <ul>
+            <li>
+              {/* TODO Lägg till korrekta länkar */}
+              <Link to='#'>Privacy</Link>
+            </li>
+            <li>
+              {/* TODO Lägg till korrekta länkar */}
+              <Link to='#'>Terms and conditions</Link>
+            </li>
+            <li>
+              {/* TODO Lägg till korrekta länkar */}
+              <Link to='#'>Policy</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className='footer-other-links'>
+          <ul>
+            <li>Annan länk</li>
+            <li>Annan länk</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
