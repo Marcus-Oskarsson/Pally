5f57779424ae853acc84f3c324e6c724046655a6
bjosefine <josefinebrolin95@gmail.com>
2023-11-14 15:03:32 +0100

    search friends styling done-ish

--- frontend/src/components/SearchFriends.jsx
+++ frontend/src/components/SearchFriends.jsx
@@ -1,10 +1,12 @@
+import SearchIcon from '../assets/search.png';
 export const SearchFriends = () => {
   return (
     <>
       <div className='main-container'>
         <h2>Find new friends</h2>
-        <div>
+        <div className='search-friends-container'>
           <input type='text' />
+          <img src={SearchIcon} alt='' />
         </div>
       </div>
     </>
