aead06f7ca5360fd49b5a5a7723205c253ae28a9
lopesdaluz <tlopesdaluz@gmail.com>
2023-11-14 11:21:48 +0100

    update home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import '../styles/home.scss';
+import StartPage from '../components/StartPage.jsx';
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <StartPage />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
