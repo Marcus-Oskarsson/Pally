75885a48b4f96c9bf98c9d92e7d5d328fc04deeb
MalinAlm <malin.therese.almqvist@gmail.com>
2023-11-14 14:00:27 +0100

    styling profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,12 @@
+import ProfileInfo from '../components/ProfileInfo';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <>
+      <div>Profile</div>
+      <ProfileInfo />
+    </>
+  );
 };
 
 export default Profile;
