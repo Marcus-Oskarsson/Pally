7b83d43ca4072dc081f7fb7ed32812af0a041ac7
Marcus Oskarsson <35783488+Marcus-Oskarsson@users.noreply.github.com>
2023-11-16 14:56:26 +0100

    Update context with correct user.etc...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Event from './pages/Event';
 import Friends from './pages/Friends';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import PrivateRoutes from './components/PrivateRoutes';
 import Profile from './pages/Profile';
 
 import Footer from './components/Footer';
@@ -29,11 +30,16 @@ const App = () => {
   const router = createHashRouter([
     {
       children: [
-        { element: <Home />, path: '/' },
+        {
+          element: <PrivateRoutes />,
+          children: [
+            { element: <Profile />, path: '/profile' },
+            { element: <Friends />, path: '/friends' },
+            { element: <Home />, path: '/' },
+          ],
+        },
         { element: <Login />, path: '/login' },
         { element: <Event />, path: '/event' },
-        { element: <Profile />, path: '/profile' },
-        { element: <Friends />, path: '/friends' },
       ],
       element: <Root />,
     },
