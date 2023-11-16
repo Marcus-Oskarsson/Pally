a90abb6614759bb342f0a0f96a6236784aa7ad12
bjosefine <josefinebrolin95@gmail.com>
2023-11-16 15:48:43 +0100

    testing friends

--- backend/routes/friend.js
+++ backend/routes/friend.js
@@ -7,13 +7,31 @@ router.get('/friends', async (req, res) => {
     const friends = await client.query(`
     SELECT f.friendId, u2.userFirstName AS firstName, u2.userLastName AS lastName
     FROM friend f
-    INNER JOIN userInfo u2 ON f.user2Id = u2.userId
-
-  `);
+    INNER JOIN userInfo u2 ON f.user2Id = u2.userId`);
     res.json(friends.rows);
   } catch (error) {
     console.error('failed', error);
     res.status(500).json({ error: 'failed' });
+  }
+});
+
+router.get('/friends/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    console.log(userId);
+    const friends = await client.query(
+      `
+    SELECT f.friendId, u2.userFirstName AS firstName, u2.userLastName AS lastName
+    FROM friend f
+    INNER JOIN userInfo u2 ON f.user2Id = u2.userId
+    WHERE f.user1Id = $1
+    `,
+      [userId]
+    );
+    res.json(friends.rows);
+  } catch (error) {
+    console.error('failed', error);
+    res.status(500).json({ error: `Failed with id ${userId}` });
   }
 });
 
