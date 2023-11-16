553e6ce23f5f51429578ecc286aa7e592b4835d2
StinaNorqvist <norqvist.stina@gmail.com>
2023-11-16 11:26:29 +0100

    First draft of login

--- backend/index.js
+++ backend/index.js
@@ -4,37 +4,11 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+
+const login = require('./routes/login');
+app.use(login);
+
 const PORT = process.env.PORT || 3000;
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.PGURI,
-});
-
-client.connect();
-
-async function main() {
-  try {
-    const { rows } = await client.query('SELECT * FROM userInfo');
-    console.log('Table created successfully!');
-    console.log(rows);
-  } catch (error) {
-    console.error('Error connecting!');
-    console.error(error);
-  }
-}
-
-main();
-
-// Get all poems
-app.get('/', async (_request, response) => {
-  console.log('GET-request received for /');
-  const { rows } = await client.query('SELECT * FROM userInfo');
-
-  console.log('GET-request received for /api');
-
-  response.send(rows);
-});
 
 app.listen(PORT, () => {
   console.log('Dicker is up n running. GLHF.');
---
+++ backend/routes/login.js
@@ -0,0 +1,40 @@
+// gömmer lösenord
+// import bcrypt from 'bcrypt';
+// // access tolken
+// import crypto from 'crypto';
+
+const express = require('express');
+const router = express.Router();
+// import { Router } from 'express';
+// const router = Router();
+
+const client = require('../connection');
+
+router.post('/login', async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const sql = `SELECT * FROM userInfo WHERE userEmail = $1`;
+
+  // const newAccesstoken = crypto.randomBytes(128).toString('hex');
+  // const hashedPassword = bcrypt.hashSync(password, salt);
+  // salt = krypterar lösen på samma sätt varje gång
+
+  try {
+    await client.query(sql, [email], async (error, results) => {
+      if (error) {
+        throw error;
+      }
+      console.log('Login, check password');
+
+      if (password === results.rows[0].userpassword) {
+        console.log('Correct password');
+        res.send(results.rows[0]);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
