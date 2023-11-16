192321d08c62b5c7aaaf23dce2c9df4674d6fd27
bjosefine <josefinebrolin95@gmail.com>
2023-11-16 16:05:18 +0100

    user friends done-ish

--- backend/routes/friend.js
+++ backend/routes/friend.js
@@ -21,11 +21,14 @@ router.get('/friends/:userId', async (req, res) => {
     console.log(userId);
     const friends = await client.query(
       `
-    SELECT f.friendId, u2.userFirstName AS firstName, u2.userLastName AS lastName
-    FROM friend f
-    INNER JOIN userInfo u2 ON f.user2Id = u2.userId
-    WHERE f.user1Id = $1
-    `,
+      SELECT f.friendId,
+             CASE WHEN f.user1Id = $1 THEN u2.userFirstName ELSE u1.userFirstName END AS firstName,
+             CASE WHEN f.user1Id = $1 THEN u2.userLastName ELSE u1.userLastName END AS lastName
+      FROM friend f
+      INNER JOIN userInfo u1 ON f.user1Id = u1.userId
+      INNER JOIN userInfo u2 ON f.user2Id = u2.userId
+      WHERE f.user1Id = $1 OR f.user2Id = $1
+      `,
       [userId]
     );
     res.json(friends.rows);
