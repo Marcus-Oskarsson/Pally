484d80c51f906c6e505103717616a299c3461ec2
lopesdaluz <tlopesdaluz@gmail.com>
2023-11-20 15:37:59 +0100

    added userid picture for friends, event and privacy text

--- frontend/src/components/PrivacyText.jsx
+++ frontend/src/components/PrivacyText.jsx
@@ -5,11 +5,37 @@ const PrivacyText = () => {
 
       {/* Tatiana: */}
       <h2>Contact Information</h2>
-      <p></p>
+      <p>
+        Namn: Pally AB 
+        E-post: info@Pally.se 
+        
+        Telefon: 031-123456 Varbergsgatan 1, 
+        123 45 Mölndal
+      </p>
 
       {/* Tatiana: */}
-      <h2>Vilka uppgifter vi lagrar och hur länge</h2>
-      <p></p>
+      <h2>What data do we store and for how long?</h2>
+      <p>
+        We collect contact information to distinguish users from each other. We
+        gather contact details to communicate with users regarding events. We
+        store the information in a safe way so our users can use our service.
+        The personal data is stored for as long the user is a member.
+
+        Name: We gather your first name to uniquely identify users. 
+
+        Lastname: We collect your surname for the purpose of user distinction. 
+
+        Email: The collection of email addresses is intended for communication with users regarding
+        events. 
+
+        Address: Addresses are collected to facilitate the mailing of
+        event related correspondence. 
+
+        Securitynumber- At all events alcohol is served, so we need to ensure that you are over 18 years old. We require
+        your full social security number so we can verify that you are of legal
+        age.
+      </p>
+      
 
       {/* Johan: */}
       <h2>Vårt ändamål med insamlingen av uppgifterna</h2>
