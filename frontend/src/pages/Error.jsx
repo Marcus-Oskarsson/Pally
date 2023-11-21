a1d2e1e3dc73e5cd5f566300c2dc4e384fd2e189
Marcus Oskarsson <35783488+Marcus-Oskarsson@users.noreply.github.com>
2023-11-21 10:55:59 +0100

    Fix image component and adds amazing 404 page

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,15 +1,43 @@
+import { useState, useEffect } from 'react';
+import TextTransition, { presets } from 'react-text-transition';
+
 import ImageComponent from '../components/ImageComponent';
-// import './Error.css';
+import '../styles/error.scss';
+
+const TEXTS = [
+  '404',
+  'Oops!',
+  'The page',
+  "you're",
+  'looking',
+  'for',
+  "doesn't",
+  'exist.',
+];
 
 const Error = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIndex((index) => index + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <>
-      <h1 className='error-heading'>404</h1>
-      <p className='error-message'>
-        Oops! The page you&apos;re looking for doesn&apos;t exist.
-      </p>
+    <div className='error-page'>
+      <div className='error-text-container'>
+        <h1 className='error-heading'>404</h1>
+        {/* <p className='error-message'>
+          Oops! The page you&apos;re looking for doesn&apos;t exist.
+        </p> */}
+        <TextTransition className='text' springConfig={presets.molasses}>
+          {TEXTS[index % TEXTS.length]}
+        </TextTransition>
+      </div>
       <ImageComponent imageName='not-found' className='error-image' />
-    </>
+    </div>
   );
 };
 
