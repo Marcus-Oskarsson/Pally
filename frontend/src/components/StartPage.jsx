66342709c38ff5156cda3ddef3d4c41ca72df54e
lopesdaluz <tlopesdaluz@gmail.com>
2023-11-21 14:55:59 +0100

    changed styling on home a little and added so max 6 friends can be shown

--- frontend/src/components/StartPage.jsx
+++ frontend/src/components/StartPage.jsx
@@ -49,27 +49,29 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='main-content'>
+    <>
       <div className='friends-container'>
-        {friendInfo.map((friends) => (
+        {friendInfo.slice(0, 6).map((friends) => (
           <div key={friends.friendid} className='friend-box'>
             <div className='friend-round-border'>
-              <img className='picture' src={friends.userimgurl} />
+              <img
+                className='picture'
+                src={friends.userimgurl}
+                alt={friends.firstname}
+              />
             </div>
-            <div className='friend-name'>
-              {friends.firstname} {friends.lastname}
-            </div>
+            <div className='friend-name'>{friends.firstname}</div>
           </div>
         ))}
       </div>
 
-      {/*Active Events */}
+      {/* Active Events */}
       <div className='event-main'>
         <h2 className='active-event-title'>Active Events</h2>
         {eventInfo.length > 0 ? (
           eventInfo.map((event) => (
-            <div key={'eventInfo-' + event.eventid}>
-              <div className='box'>
+            <div className='box' key={'eventInfo-' + event.eventid}>
+              <div className='box-img'>
                 <img
                   className='picture-one'
                   src={event.eventimage}
@@ -86,28 +88,21 @@ const Home = () => {
           <div className='no-active-events'>No active events</div>
         )}
 
-        {/*Upcoming Events */}
+        {/* Upcoming Events */}
         <h3 className='upcoming-event-title'>Upcoming Events</h3>
         <p className='event-description'>
           Discover exciting events and create memorable experiences.
         </p>
         {eventUpcoming.map((event) => (
-          <div key={'eventUpcoming-' + event.eventid}>
-            <Link to='/event'>
-              <div className='box'>
-                <img
-                  className='picture-one'
-                  src={event.eventimage}
-                  alt='Event'
-                />
-              </div>
-              <div className='event-name'>{event.eventname}</div>
-            </Link>
+          <div className='box' key={'eventUpcoming-' + event.eventid}>
+            <div className='box-img'>
+              <img className='picture-one' src={event.eventimage} alt='Event' />
+            </div>
+            <div className='event-name'>{event.eventname}</div>
           </div>
         ))}
       </div>
-    </div>
+    </>
   );
 };
-
 export default Home;
