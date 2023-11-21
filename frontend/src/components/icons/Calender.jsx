14ad6dec4d2c8dc4484faea2a87758a29ff431dd
StinaNorqvist <norqvist.stina@gmail.com>
2023-11-21 14:51:22 +0100

    Footer menu

---
+++ frontend/src/components/icons/Calender.jsx
@@ -0,0 +1,20 @@
+export function Calender(props) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='1.7em'
+      height='1.7em'
+      viewBox='0 0 24 24'
+      {...props}
+    >
+      <path
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='1.5'
+        d='M4 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V7zm12-4v4M8 3v4m-4 4h16m-9 4h1m0 0v3'
+      ></path>
+    </svg>
+  );
+}
