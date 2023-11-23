66cb0c6e0698c5006de3c0b250f37e124a874450
MalinAlm <malin.therese.almqvist@gmail.com>
2023-11-23 11:17:34 +0100

    hide settings button on friens page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,20 +1,23 @@
 import { useState } from 'react';
-
+import { useParams } from 'react-router-dom';
 import ProfileInfo from '../components/ProfileInfo';
 import ProfileSettings from '../components/ProfileSettings';
 
 const Profile = () => {
   const [toggleProfileSetting, setToggleProfileSettings] = useState(false);
+  const { id } = useParams();
   return (
     <>
       <div className='settings-button'>
         {toggleProfileSetting ? <ProfileSettings /> : <ProfileInfo />}
         <div className='btn-container'>
-          <button
-            onClick={() => setToggleProfileSettings(!toggleProfileSetting)}
-          >
-            {toggleProfileSetting ? 'Profile info' : 'Profile settings'}
-          </button>
+          {!isNaN(id) ? null : (
+            <button
+              onClick={() => setToggleProfileSettings(!toggleProfileSetting)}
+            >
+              {toggleProfileSetting ? 'Profile info' : 'Profile settings'}
+            </button>
+          )}
         </div>
       </div>
     </>
